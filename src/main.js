f7d7e1a3cbe76e9e760f86d0cc56813b0c9244e4
Chetana Hiremath <cchiremath@tekion.com>
2021-05-13 13:28:57 +0530

    mvctodoapp done

--- src/main.js
+++ src/main.js
@@ -1,69 +1,55 @@
-const globalInput = document.getElementById("box");
-const container = document.querySelector(".container");
-const addButton = document.querySelector(".addButton");
-const editButton = document.querySelector(".editButton");
-const removeButton = document.querySelector(".removeButton");
-const checkBox = document.querySelector(".checkBox");
-
-class addEventListener {
+class Model {
   constructor() {
-    document.addEventListener(
-      "click",
-      (e) => {
-        console.log(e.target);
-        if (this.hasClass(e.target, "editButton")) {
-          console.log(e.target);
-          this.edit(e);
-        } else if (this.hasClass(e.target, "removeButton")) {
-          console.log(e.target);
-          this.remove(e);
-        } else if (this.hasClass(e.target, "checkBox")) {
-          console.log(e.target);
-          this.complete(e);
-        }
-      },
-      false
-    );
+    this.input = document.querySelector(".inputbox");
+    this.todoList = [];
   }
-  hasClass = (elem, className) => {
-    return elem.classList.contains(className);
-  };
+  bindtodoListChanged(callback) {
+    this.ontodoListChanged = callback;
+  }
 
-  edit = (e) => {
-    console.log(e);
-    const uniqid = e.target.uniqid;
-    console.log(uniqid);
+  addNewTodo() {
+    const uniqid = new Date().getTime();
+    if (this.input.value !== "") {
+      this.input.value;
+      this.todoList.push({ uniqid, value: this.input.value });
+      console.log(this.todoList);
+      this.input.value = "";
+    }
+    this._commit(this.todoList);
+  }
+  _commit(todos) {
+    this.ontodoListChanged(todos);
+  }
+
+  editButton(uniqid, _temporaryTodoText) {
     const input = document.getElementById("inputbox" + uniqid);
-    const checkBox = document.getElementById("checkBox" + uniqid);
     const editButton = document.getElementById("editButton" + uniqid);
+    const checkBox = document.getElementById("checkBox" + uniqid);
 
-    //input.disabled = !input.disabled;
     if (input.contentEditable == "false") {
-      editButton.innerHTML = "Edit";
-      checkBox.classList.remove("hideContent");
-      input.contentEditable = "true";
-    } else {
       checkBox.classList.add("hideContent");
       editButton.innerHTML = "Submit";
+      input.contentEditable = "true";
+    } else {
+      editButton.innerHTML = "Edit";
+      checkBox.classList.remove("hideContent");
+
       input.contentEditable = "false";
     }
-  };
+  }
 
-  remove = (e) => {
-    console.log(e);
-    const uniqid = e.target.uniqid;
+  removeButton = (uniqid) => {
     console.log(uniqid);
-    const itemContainer = document.getElementById("item" + uniqid);
-    container.removeChild(itemContainer);
+    this.todoList = this.todoList.filter((todos) => todos.uniqid !== uniqid);
+    this._commit(this.todoList);
   };
 
-  complete = (e) => {
-    console.log(e);
-    const uniqid = e.target.uniqid;
+  completeButton = (uniqid) => {
     console.log(uniqid);
     const input = document.getElementById("inputbox" + uniqid);
     const editButton = document.getElementById("editButton" + uniqid);
     const checkBox = document.getElementById("checkBox" + uniqid);
+
     if (checkBox.checked) {
       editButton.classList.add("hideContent");
     } else {
@@ -73,58 +59,155 @@ class addEventListener {
   };
 }
 
-class todoItem {
-  constructor(textInput) {
-    var uniqid = new Date().getTime();
-    this.input = document.createElement("div");
-    this.input.setAttribute("contenteditable", "false");
-    this.input.setAttribute("id", "inputbox" + uniqid);
-    this.input.disabled = true;
-
-    this.input.innerHTML = textInput;
-    this.input.classList.add("textarea");
-
-    this.itemContainer = document.createElement("div");
-    this.itemContainer.classList.add("item");
-    this.itemContainer.setAttribute("id", "item" + uniqid);
-    this.itemContainer.uniqid = uniqid;
-
-    this.editButton = document.createElement("button");
-    this.editButton.setAttribute("id", "editButton" + uniqid);
-    this.editButton.uniqid = uniqid;
-    this.editButton.innerHTML = "Edit";
-    this.editButton.classList.add("editButton");
-
-    this.removeButton = document.createElement("button");
-    this.removeButton.setAttribute("id", "removeButton" + uniqid);
-    this.removeButton.uniqid = uniqid;
-    this.removeButton.innerHTML = "Remove";
-    this.removeButton.classList.add("removeButton");
-
-    this.checkBox = document.createElement("input");
-    this.checkBox.setAttribute("id", "checkBox" + uniqid);
-    this.checkBox.uniqid = uniqid;
-    this.checkBox.type = "checkbox";
-    this.checkBox.classList.add("checkBox");
-
-    this.itemContainer.appendChild(this.checkBox);
-    this.itemContainer.appendChild(this.input);
-    this.itemContainer.appendChild(this.editButton);
-    this.itemContainer.appendChild(this.removeButton);
-    container.appendChild(this.itemContainer);
-  }
-}
-
-class todoList {
+class View {
   constructor() {
-    addButton.addEventListener("click", this.addNewTodo);
-    new addEventListener();
+    this.input = document.createElement("input");
+    this.input.classList.add(".itemInput");
+    this.input.type = "text";
+    this.addButton = document.querySelector(".addButton");
+
+    this.container = document.querySelector(".container");
+    this.todoList = document.createElement("ul", "todoList");
+    this.todoList.classList.add("todoList");
+    this.input.name = "todos";
+    this.input.disabled = true;
+    this.container.append(this.todoList);
+
+    this._temporaryTodoText = "";
+    this._initLocalListeners();
   }
-  addNewTodo() {
-    if (globalInput.value !== "") {
-      new todoItem(globalInput.value);
-      globalInput.value = "";
+
+  displayTodos(todos) {
+    console.log(todos);
+    while (this.todoList.firstChild) {
+      this.todoList.removeChild(this.todoList.firstChild);
+    }
+    for (let i = 0; i < todos.length; i++) {
+      this.createDiv(todos[i]);
     }
   }
+
+  createDiv(todos) {
+    console.log(todos);
+    const uniqid = todos.uniqid;
+    const inputbutton = document.createElement("div");
+    inputbutton.setAttribute("contenteditable", "false");
+    inputbutton.setAttribute("id", "inputbox" + uniqid);
+    inputbutton.value = todos.value;
+    inputbutton.classList.add("textarea");
+    inputbutton.innerHTML = todos.value;
+
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("item");
+    itemContainer.setAttribute("id", "item" + uniqid);
+    itemContainer.uniqid = uniqid;
+
+    const editButton = document.createElement("button");
+    editButton.setAttribute("id", "editButton" + uniqid);
+    editButton.uniqid = uniqid;
+    editButton.innerHTML = "Edit";
+    editButton.classList.add("editButton");
+
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("id", "removeButton" + uniqid);
+    removeButton.uniqid = uniqid;
+    removeButton.innerHTML = "Remove";
+    removeButton.classList.add("removeButton");
+
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("id", "checkBox" + uniqid);
+    checkBox.uniqid = uniqid;
+    checkBox.type = "checkBox";
+    checkBox.classList.add("checkBox");
+
+    const li = document.createElement("li");
+    li.id = "itemContainer" + uniqid;
+    li.uniqid = todos.uniqid;
+
+    li.append(editButton);
+    li.append(removeButton);
+    li.append(checkBox);
+    li.append(inputbutton);
+    this.todoList.append(li);
+  }
+
+  _initLocalListeners() {
+    this.todoList.addEventListener("click", (event) => {
+      if (event.target.className == "editButton") {
+        this._temporaryTodoText = event.target.innerText;
+      }
+    });
+  }
+
+  bindaddNewTodo(handler) {
+    this.addButton.addEventListener("click", (event) => {
+      handler();
+    });
+  }
+
+  bindeditButton(handler) {
+    this.todoList.addEventListener("click", (event) => {
+      console.log("this.editButton");
+      if (this._temporaryTodoText) {
+        const uniqid = parseInt(event.target.uniqid);
+        handler(uniqid, this._temporaryTodoText);
+        this._temporaryTodoText = "";
+      }
+    });
+  }
+
+  bindremoveButton(handler) {
+    this.todoList.addEventListener("click", (event) => {
+      console.log("this.removeButton");
+      if (event.target.className == "removeButton") {
+        const uniqid = parseInt(event.target.uniqid);
+        handler(uniqid);
+      }
+    });
+  }
+
+  bindcompleteButton(handler) {
+    this.todoList.addEventListener("change", (event) => {
+      console.log("this.completeButton");
+      if (event.target.className == "checkBox") {
+        const uniqid = parseInt(event.target.uniqid);
+        handler(uniqid);
+      }
+    });
+  }
 }
-new todoList();
+class Controller {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+
+    this.model.bindtodoListChanged(this.ontodoListChanged);
+    this.view.bindaddNewTodo(this.handleaddNewTodo);
+    this.view.bindeditButton(this.handleeditButton);
+    this.view.bindremoveButton(this.handleremoveButton);
+    this.view.bindcompleteButton(this.handlecompleteButton);
+
+    // Display initial todos
+    this.ontodoListChanged(this.model.todoList);
+  }
+  ontodoListChanged = (todos) => {
+    this.view.displayTodos(todos);
+  };
+
+  handleaddNewTodo = () => {
+    this.model.addNewTodo();
+  };
+
+  handleeditButton = (_temporaryTodoText) => {
+    this.model.editButton(_temporaryTodoText);
+  };
+
+  handleremoveButton = (uniqid) => {
+    this.model.removeButton(uniqid);
+  };
+
+  handlecompleteButton = (uniqid) => {
+    this.model.completeButton(uniqid);
+  };
+}
+const app = new Controller(new Model(), new View());
